c000f07bfeeb2298868af839a3b91e18e611d31f
S3R43o3 <67480273+sera619@users.noreply.github.com>
2022-07-21 13:38:55 +0200

    update bypass

--- BypassPowerShell.ps1
+++ BypassPowerShell.ps1
@@ -1,6 +1,8 @@
 Clear-Host 
 Write-Host "-: PowerShell Bypass :-" -ForegroundColor Red
-Write-Host "Please notice that ONLY for this PowerShell-session, all script restricts are disabled!`r`nAs long this session is active. Disable Bypass automaticly." -ForegroundColor Yellow
+Write-Host "-by S3R43o3 2022-" -ForegroundColor Blue
+Write-Host "Please notice that ONLY for this PowerShell-session, all script restricts are disabled,`r`nas long this session is active! Disable Bypass automaticly." -ForegroundColor Yellow
+Write-Host " "
 Start-Sleep -Seconds 2
 powershell.exe -ExecutionPolicy Bypass
 Exit-PSSession
